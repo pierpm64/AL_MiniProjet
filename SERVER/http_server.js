3683deb52ac00fed523649d6d65bba3dd8aba32b
Florane Ployart <florane.ployart@gmail.com>
2021-04-29 20:00:25 +0200

    added small express server on 8888

---
+++ SERVER/http_server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//route principale:
+app.get('/', function(req, res , next) {
+    res.setHeader('Content-Type', 'text/html');
+    res.write("<html> <body><form name = 'form1' method= 'POST' action = '/name'/>");
+    res.write('<p>Entrez votre nom</p>');
+    res.write('<input type = "text" name = "name"/><input type="submit" value = "submit"/></form>');
+    res.write("</body></html>");
+    res.end();
+});
+
+
+app.post('/name', function(req, res , next) {
+    res.setHeader('Content-Type', 'text/html');
+	let name = req.body.name;
+    res.write("<html> <body>");
+    res.write('<p>Bonjour '+name+'</p>');
+    res.write("</body></html>");
+    res.end();
+});
+
+
+app.listen(8888 , function () {
+    console.log("simple express node server listening at 8888");
+});
